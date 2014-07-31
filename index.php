a50c3a033cc6784eae016dd236a222ff65df373e
naarika <naarika@rambler.ru>
2014-07-31 11:25:35 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title> title </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+</head>
+
+
+<body>
+
+<FORM  METHOD=POST action="Chekway.php">
+    <select name="piece" size="1">
+        <option  value="Rock">Rock</option>
+        <option  value="Bishop">Bishop</option>
+    </select>
+<!--     <select name="website_string"> 
+        <option value="" selected="selected"></option>
+        <option VALUE="abc"> ABC</option>
+        <option VALUE="def"> def</option>
+        <option VALUE="hij"> hij</option>   
+    </select> -->
+
+    <INPUT NAME="leter" TYPE="text" VALUE="e" placeholder="e">
+    <INPUT NAME="number" TYPE="text" VALUE="4" placeholder="4">
+    <INPUT NAME="submit" TYPE="submit" VALUE="ход">
+    <br><br>
+</FORM>
+
+
+
+</body>
+</html>
